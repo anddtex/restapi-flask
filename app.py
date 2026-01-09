ee9958bfc678fa79cc47a593cdabe94e9160616d
anddtex <anddtex@gmail.com>
2026-01-09 18:05:55 -0300

    fix: formatação de acordo com flake8

--- app.py
+++ app.py
@@ -4,20 +4,23 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class Users(Resource):
     def get(self):
         return {'message': 'Usuario 1'}
 
+
 class User(Resource):
-    def get(self):
+    def post(self):
         return {'message': 'teste'}
-    
-    def get(self):
+
+    def get(self, cpf):
         return {'message': 'CPF'}
 
 
+# determinando os endpoints
 api.add_resource(Users, '/users')
-api.add_resource(User, '/user', '/user/<string:cpf>') #determinando os endpoints
+api.add_resource(User, '/user', '/user/<string:cpf>')
 
 if __name__ == '__main__':
     app.run(debug=True)
