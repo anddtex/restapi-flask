265a4722e674a01ff80d2c17ef481b7102fcbbe4
anddtex <anddtex@gmail.com>
2026-01-21 16:15:44 -0300

    refactor: usando aplicação de fatoramento

--- application/app.py
+++ application/app.py
@@ -1,19 +1,8 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, reqparse, Api
-from flask_mongoengine import MongoEngine
+from flask import jsonify
+from flask_restful import Resource, reqparse
 from mongoengine import NotUniqueError
+from .model import UserModel
 import re
-
-app = Flask(__name__)
-
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'users',
-    'host': 'mongodb',
-    'port': 27017,
-    'username': 'admin',
-    'password': 'admin',
-    'authentication_source': 'admin'
-}
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('first_name',
@@ -41,18 +30,6 @@ _user_parser.add_argument('birth_date',
                           required=True,
                           help="Esse campo não pode ficar em branco"
                           )
-
-
-api = Api(app)
-db = MongoEngine(app)
-
-
-class UserModel(db.Document):
-    cpf = db.StringField(required=True, unique=True)
-    first_name = db.StringField(max_length=50)
-    last_name = db.StringField(max_length=50)
-    email = db.EmailField(required=True)
-    birth_date = db.DateTimeField(required=True)
 
 
 class Users(Resource):
@@ -111,11 +88,3 @@ class User(Resource):
             return jsonify(response)
         # return jsonify(UserModel.objects(cpf=cpf))
         return {'message': 'Usuario inexistente na base de dados!'}, 400
-
-
-# determinando os endpoints
-api.add_resource(Users, '/users')
-api.add_resource(User, '/user', '/user/<string:cpf>')
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
