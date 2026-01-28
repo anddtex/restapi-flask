936ff6d50d476a05e82a88dae2a95ea2c0d59df6
anddtex <anddtex@gmail.com>
2026-01-28 18:43:55 -0300

    test: testando o get user

--- tests/test_application.py
+++ tests/test_application.py
@@ -42,3 +42,17 @@ class TestApplication():
         assert response.status_code == 400
         assert b"invalido" in response.data
 
+    def test_get_user(self, client, valid_user, invalid_user):
+        response = client.get('/user/%s' % valid_user["cpf"])
+        assert response.status_code == 200
+        assert response.json[0]["first_name"] == "James"
+        assert response.json[0]["last_name"] == "Rhodes"
+        assert response.json[0]["cpf"] == "425.403.090-89"
+        assert response.json[0]["email"] == "warmachine@avengers.com"
+        # birth_date = response.json[0]["birth_date"]["$date"]
+        # assert birth_date == "1964-11-29T00:00:00Z"
+        assert "birth_date" in response.json[0]
+
+        response = client.get('/user/%s' % invalid_user["cpf"])
+        assert response.status_code == 400
+        assert b"inexistente" in response.data
